719761459635d5fbbb395f037073bffb66eb4b5a
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-25 11:48:37 +0200

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+alert('Hello, ' + name); // Wiadomość wyświetlana w oknie zawierająca zmienną 'name'
 console.log('Hello, ' + name);
+/*
+	Powyższa funkcja wyświetla w konsoli
+	tekst - "Hello" oraz imię użytkownika
+	pobrane za pomocą 'prompt'
+*/
