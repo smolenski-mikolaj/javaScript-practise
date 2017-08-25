ebbd05dc11d484da419a59effbb4583920ffa15d
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-25 12:42:58 +0200

    triangle script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name); // Wiadomość wyświetlana w oknie zawierająca zmienną 'name'
-console.log('Hello, ' + name);
-/*
-	Powyższa funkcja wyświetla w konsoli
-	tekst - "Hello" oraz imię użytkownika
-	pobrane za pomocą 'prompt'
-*/
+var a = prompt('Enter triangle base'),
+	h = prompt('Enter triangle height'),
+	triangleArea;
+
+triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
