81985955916db8b226ac691b58ff2907a0bc2b28
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-27 13:30:31 +0200

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'css/style.css': 'sass/style.sass'
+  			}
+  		}
+  	},
+	
+	watch: {
+		scripts: {
+			files: ['sass/*.sass'],
+			tasks: ['sass'],
+			options: {
+				spawn: false,
+			},
+		},
+	},
+  
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'watch']);
+};
