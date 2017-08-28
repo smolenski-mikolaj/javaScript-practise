80a1cb98aac790a22eb67c557e61e8040ef19024
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-28 21:13:03 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 var a = 3,
 	b = 2,
 	value;
-
-if ( isNaN(a) == false &&  isNaN(b) == false ) {
 	
 	value = (a * a) + (2 * a * b) - (b * b);
 	console.log('Wynik: ' + value);
 	
+if ( isNaN(a) == false &&  isNaN(b) == false ) {	
 	if (value > 0) {
 		console.log('Wynik dodatni');
 	} else if (value < 0) {
